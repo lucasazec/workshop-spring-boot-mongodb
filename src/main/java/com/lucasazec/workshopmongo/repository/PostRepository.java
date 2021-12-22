32c514667f99fd6ff3d32159bf002288cac572a0
Lucas Azevedo <lucasazevedo0895@gmail.com>
2021-12-21 22:52:12 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lucasazec/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucasazec.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasazec.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
